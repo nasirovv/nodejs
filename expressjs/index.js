a84ac6f4e13621df307cab0bee835391bc342013
Mirfayz Nosirov <nasirov0017@gmail.com>
2021-06-10 13:55:52 +0300

    first commit

---
+++ expressjs/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+// Init middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
